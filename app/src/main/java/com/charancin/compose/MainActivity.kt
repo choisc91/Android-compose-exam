9876ab8fb09a12d2330c8975f9dbc194262a9057
Dev Choe <choisc91@naver.com>
2021-11-17 19:39:16 +0900

    # navigation exam. # gradle sdk version fix.

--- app/src/main/java/com/charancin/compose/MainActivity.kt
+++ app/src/main/java/com/charancin/compose/MainActivity.kt
@@ -3,35 +3,132 @@ package com.charancin.compose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 import com.charancin.compose.ui.theme.ComposeTheme
 
+private const val FIRST = "first"
+private const val SECOND = "second"
+private const val THIRD = "third"
+
 class MainActivity : ComponentActivity() {
+
+    @Preview(showBackground = true)
+    @Composable
+    fun DefaultPreview() {
+        ComposeTheme {
+            BuildScaffold()
+        }
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            ComposeTheme {
-                Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Android")
+            BuildScaffold()
+        }
+    }
+
+    @Composable
+    fun BuildScaffold() {
+        val navigationCtrl = rememberNavController()
+        Scaffold {
+            NavHost(navController = navigationCtrl, startDestination = FIRST) {
+                composable(FIRST) {
+                    BuildFirstScreen(navigationCtrl)
+                }
+                composable(SECOND) {
+                    BuildSecondScreen(navigationCtrl)
+                }
+                composable("$THIRD/{value}") {
+                    BuildThirdScreen(navigationCtrl, it.arguments?.getString("value") ?: "")
                 }
             }
+        }
+    }
+
+}
+
+@Composable
+fun BuildFirstScreen(ctrl: NavController) {
+    // 코틀린 구조 분해.
+    val (value, setValue) = remember {
+        mutableStateOf("")
+    }
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally,
+    ) {
+        Text(text = "This is first screen")
+        Spacer(modifier = Modifier.height(24.dp))
+        Button(onClick = {
+            ctrl.navigate(SECOND)
+        }) {
+            Text(text = "Move to second screen")
+        }
+        Spacer(modifier = Modifier.height(24.dp))
+        TextField(
+            value = value,
+            onValueChange = setValue,
+            placeholder = { Text(text = "input password") })
+        Spacer(modifier = Modifier.height(8.dp))
+        Button(onClick = {
+            if (value == "8878") ctrl.navigate("$THIRD/$value")
+            // todo add snack bar.
+        }) {
+            Text(text = "Move to third screen")
+        }
+
+
+    }
+}
+
+@Composable
+fun BuildSecondScreen(ctrl: NavController) {
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally,
+    ) {
+        Text(text = "This is second screen")
+        Spacer(modifier = Modifier.height(8.dp))
+        Button(onClick = {
+            ctrl.popBackStack()
+        }) {
+            Icon(imageVector = Icons.Default.ArrowBack, contentDescription = "arrow_back")
         }
     }
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    ComposeTheme {
-        Greeting("Mr.Choe")
+fun BuildThirdScreen(ctrl: NavController, password: String) {
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally,
+    ) {
+        Text(text = "This is third screen")
+        Text(text = "check it password, $password")
+        Spacer(modifier = Modifier.height(8.dp))
+        Button(onClick = {
+            ctrl.navigateUp()
+        }) {
+            Icon(imageVector = Icons.Default.ArrowBack, contentDescription = "arrow_back")
+        }
     }
 }
