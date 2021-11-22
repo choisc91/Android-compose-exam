703d8e8961b6029386723fbdf4c2dc67d5f9937d
Dev Choe <choisc91@naver.com>
2021-11-22 15:33:12 +0900

    exam app 3

--- app/src/main/java/com/charancin/compose/MainActivity.kt
+++ app/src/main/java/com/charancin/compose/MainActivity.kt
@@ -1,11 +1,14 @@
 package com.charancin.compose
 
 import android.os.Bundle
+import android.webkit.WebView
+import android.webkit.WebViewClient
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
@@ -16,20 +19,29 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.ColorFilter
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalFocusManager
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.compose.ui.viewinterop.AndroidView
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import com.charancin.compose.ui.theme.ComposeTheme
+import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.flow.asSharedFlow
+import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.launch
 import java.text.DecimalFormat
 import java.util.*
 import kotlin.concurrent.timer
@@ -48,133 +60,160 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            val viewModel = viewModel<MainViewModel>()
-            Scaffold(topBar = {
-                TopAppBar(title = { Text(text = "Timer") })
-            }) {
-                Column(
-                        modifier = Modifier.fillMaxSize(),
-                        horizontalAlignment = Alignment.CenterHorizontally,
-                ) {
-                    Spacer(modifier = Modifier.height(80.dp))
-                    // timer text.
-                    Row(
-                            horizontalArrangement = Arrangement.Center,
-                            verticalAlignment = Alignment.Bottom,
-                    ) {
-                        Text(
-                                text = "${viewModel.second.value}",
-                                style = TextStyle(fontSize = 80.sp, fontWeight = FontWeight.Bold),
-                        )
-                        Text(
-                                text = ":",
-                                style = TextStyle(fontSize = 32.sp, fontWeight = FontWeight.Bold),
-                        )
-                        Spacer(modifier = Modifier.width(8.dp))
-                        Text(
-                                text = String.format("%02d", viewModel.milli.value),
-                                style = TextStyle(fontSize = 32.sp, fontWeight = FontWeight.Bold),
-                        )
-                    }
-                    Spacer(modifier = Modifier.height(80.dp))
-                    // rap time.
-                    LazyColumn(
-                            modifier = Modifier.weight(1f)
-                    ) {
-                        // todo, items {} 를 사용하게 되면 값이 여러번 찍히는 문제 점이 있음, 확인 바람.
-                        item {
-                            viewModel.lapTime.value.forEach {
-                                Text(it)
-                            }
-                        }
+            BuildScaffold()
+        }
+    }
 
-                    }
+    @Composable
+    fun BuildScaffold() {
+        val viewModel = viewModel<MainViewModel>()
+        val focusManager = LocalFocusManager.current
+        val scaffoldState = rememberScaffoldState()
+        val (url, setUrl) = rememberSaveable {
+            mutableStateOf("https://m.naver.com")
+        }
+        val context = LocalContext.current
+        val webView = remember {
+            WebView(context).apply {
+                settings.javaScriptEnabled = true
+                webViewClient = WebViewClient()
+                loadUrl("https://m.naver.com")
+            }
+        }
 
-                    // controller.
-                    Row(
-                            modifier = Modifier
-                                    .padding(16.dp)
-                                    .fillMaxWidth(),
-                            horizontalArrangement = Arrangement.SpaceBetween,
-                    ) {
-                        FloatingActionButton(
-                                onClick = {
-                                    viewModel.reset()
-                                },
-                                backgroundColor = Color.Red,
-                        ) {
-                            Icon(imageVector = Icons.Default.Refresh, contentDescription = "reset", tint = Color.White)
-                        }
-                        FloatingActionButton(
-                                onClick = {
-                                    val isRunning = viewModel.isRunning.value
-                                    if (isRunning) viewModel.pause() else viewModel.start()
-                                },
-                        ) {
-                            val isRunning = viewModel.isRunning.value
-                            Icon(
-                                    imageVector = if (isRunning) Icons.Default.Close else Icons.Default.PlayArrow,
-                                    contentDescription = if (isRunning) "pause" else "start"
-                            )
-                        }
-                        FloatingActionButton(
-                                onClick = {
-                                    viewModel.record()
-                                },
-                                backgroundColor = Color.Blue,
-                        ) {
-                            Icon(imageVector = Icons.Default.Create, contentDescription = "record", tint = Color.White)
-                        }
-                    }
+        // Composable 안에서 코루틴 스코프 사용 시 주의.
+//        val scope = rememberCoroutineScope()
+        LaunchedEffect(Unit) {
+            viewModel.undoFlow.collectLatest {
+                if (webView.canGoBack()) {
+                    webView.goBack()
+                } else {
+                    scaffoldState.snackbarHostState.showSnackbar("마지막")
                 }
+            }
+        }
+
+        LaunchedEffect(Unit) {
+            viewModel.redoFlow.collectLatest {
+                if (webView.canGoForward()) {
+                    webView.canGoForward()
+                } else {
+                    scaffoldState.snackbarHostState.showSnackbar("첫번째")
+                }
+            }
+        }
+
+        Scaffold(
+                scaffoldState = scaffoldState,
+                topBar = {
+                    TopAppBar(
+                            title = { Text("Web app") },
+                            actions = {
+                                IconButton(
+                                        onClick = {
+                                            viewModel.undo()
+                                            // todo set back arrow action.
+                                        }
+                                ) {
+                                    Icon(
+                                            imageVector = Icons.Default.ArrowBack,
+                                            contentDescription = "back",
+                                            tint = Color.White,
+                                    )
+                                }
+                                IconButton(
+                                        onClick = {
+                                            viewModel.redo()
+                                            // todo set forward arrow action.
+                                        }
+                                ) {
+                                    Icon(
+                                            imageVector = Icons.Default.ArrowForward,
+                                            contentDescription = "forward",
+                                            tint = Color.White,
+                                    )
+                                }
+                            }
+                    )
+                }
+        ) {
+            //  todo set webview
+            Column(modifier = Modifier
+                    .padding(16.dp)
+                    .fillMaxSize()
+            ) {
+                OutlinedTextField(
+                        modifier = Modifier.fillMaxWidth(),
+                        value = url,
+                        onValueChange = setUrl,
+                        label = { Text(text = "https://") },
+                        keyboardOptions = KeyboardOptions(imeAction = ImeAction.Search),
+                        keyboardActions = KeyboardActions(onSearch = {
+                            focusManager.clearFocus()
+                            viewModel.url.value = url
+                        })
+                )
+                Spacer(modifier = Modifier.height(16.dp))
+                // Composable view 가 아닌 일반 android 에서 제공하는 View 를 사용하는 방법.
+                AndroidView(
+                        modifier = Modifier.fillMaxSize(),
+                        factory = {
+                            // context 가 들고오고, Android view 에 context 를 제공 해줘야한다.
+                            // Android web view.
+                            webView
+                        },
+                        // 코루틴 스코프을 제공하지 않아 별도 값 처리.
+                        update = { webView ->
+                            webView.loadUrl(viewModel.url.value)
+                            // 화면 갱신 부분. composition 이 다시 실행 될 때는 update 발생 함.
+                            // 비워 두게 되면 factory 따라 감.
+                            // 반복적으로 실행되는 오류가 있음.
+//                            scope.launch {
+//                                viewModel.undoFlow.collectLatest {
+//                                    if (webView.canGoBack()) {
+//                                        webView.goBack()
+//                                    } else {
+//                                        scaffoldState.snackbarHostState.showSnackbar("마지막")
+//                                    }
+//                                }
+//                            }
+//                            scope.launch {
+//                                viewModel.redoFlow.collectLatest {
+//                                    if (webView.canGoForward()) {
+//                                        webView.canGoForward()
+//                                    } else {
+//                                        scaffoldState.snackbarHostState.showSnackbar("첫번째")
+//                                    }
+//                                }
+//                            }
+                        }
+                )
             }
         }
     }
 }
 
 class MainViewModel : ViewModel() {
+    val url = mutableStateOf("https://m.naver.com")
 
-    private var time = 0
+    private val _undoFlow = MutableSharedFlow<Boolean>()
 
-    private var timeTask: Timer? = null
+    val undoFlow = _undoFlow.asSharedFlow()
 
-    private val _lapTimes = mutableStateOf(mutableListOf<String>())
+    private val _redoFlow = MutableSharedFlow<Boolean>()
 
-    val lapTime: State<List<String>> = _lapTimes
+    val redoFlow = _redoFlow.asSharedFlow()
 
-    private val _second = mutableStateOf(0)
-    val second: State<Int> = _second
 
-    private val _milli = mutableStateOf(0)
-    val milli: State<Int> = _milli
-
-    private val _isRunning = mutableStateOf(false)
-    val isRunning: State<Boolean> = _isRunning
-
-    fun start() {
-        _isRunning.value = true
-        timeTask = timer(period = 10) {
-            time++
-            _second.value = time / 100
-            _milli.value = time % 100
+    fun undo() {
+        viewModelScope.launch {
+            _undoFlow.emit(true)
         }
     }
 
-    fun pause() {
-        _isRunning.value = false
-        timeTask?.cancel()
-    }
-
-    fun reset() {
-        timeTask?.cancel()
-        time = 0
-        _isRunning.value = false
-        _second.value = 0
-        _milli.value = 0
-        _lapTimes.value.clear()
-    }
-
-    fun record() {
-        _lapTimes.value.add("#${_lapTimes.value.size}RAP ${_second.value}:${_milli.value}")
+    fun redo() {
+        viewModelScope.launch {
+            _redoFlow.emit(true)
+        }
     }
 }
